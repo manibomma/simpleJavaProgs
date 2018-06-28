63b694fec8cf1e64a5566bb7e2fc7b01025ba530
Mani Bomma <Manikanta.Bomma@Teradata.com>
2018-06-28 13:01:57 +0530

    Added constructor to enum

--- EnumSample.java
+++ EnumSample.java
@@ -1,6 +1,11 @@
 enum Beer {
 	KF, RC, FO;  // List of constants should be on first line
 	
+	// Enum can have contructor. This constructor will be executed for 
+	// every enum constant separately at the time of loading class
+	Beer() {
+		System.out.println("Enum's Constructor");
+	}
 	// Enum can have main() method. So enum can be invoked directly from cmd prompt
 	/*
 	public static void main(String[] args) {
@@ -21,3 +26,17 @@ class EnumSample {
 		}
 	}
 }
+
+
+/* OUTPUT :
+******************************************************************************************
+******************************************************************************************
+***				Enum's Constructor													   ***
+***				Enum's Constructor													   ***
+***				Enum's Constructor													   ***
+***				KF																	   ***
+***				RC																	   ***
+***				FO																	   ***
+******************************************************************************************
+******************************************************************************************
+*/
